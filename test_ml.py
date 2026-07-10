8a937eacd64002f30856f9cbe0a7241239c5c9a9
Harshini120 <717825p120@kce.ac.in>
2026-07-10 16:37:47 +0530

    Added complete Ai symptom checker

--- test_ml.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-import sklearn
-
-print("Everything is working!")
--- train_model.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-data = pd.read_csv("../dataset/Symptom2Disease.csv")
-
-print(data.head())
-print(data.columns)
-print(data.shape)
