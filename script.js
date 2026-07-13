694916a3d82affe3355097812da15729f161a936
Kanishkha K <anjanatinku12@gmail.com>
2026-07-13 09:33:43 +0530

    Added HTML CSS and JS files

---
+++ script.js
@@ -0,0 +1,22 @@
+async function checkSymptom() {
+    let symptom = document.getElementById("symptom").value;
+
+    if (symptom === "") {
+        document.getElementById("result").innerHTML = "Please enter a symptom.";
+        return;
+    }
+
+    let response = await fetch("http://127.0.0.1:5000/predict", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            symptom: symptom
+        })
+    });
+
+    let data = await response.json();
+
+    document.getElementById("result").innerHTML = data.prediction;
+}
